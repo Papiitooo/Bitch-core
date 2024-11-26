c2fc2b5fa6b8d87d3e7659a1c2729f1d819b24e4
Papiitoo <165153267+Papiitooo@users.noreply.github.com>
2024-11-26 18:13:29 +0100

    Update protocol_params.go

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -163,7 +163,7 @@ var Bls12381MultiExpDiscountTable = [128]uint64{1200, 888, 764, 641, 594, 547, 5
 
 var (
 	DifficultyBoundDivisor = big.NewInt(2048)   // The bound divisor of the difficulty, used in the update calculations.
-	GenesisDifficulty      = big.NewInt(1) // Difficulty of the Genesis block.
-	MinimumDifficulty      = big.NewInt(1) // The minimum that the difficulty may ever be.
+	GenesisDifficulty      = big.NewInt(1000000) // Difficulty of the Genesis block.
+	MinimumDifficulty      = big.NewInt(1000000) // The minimum that the difficulty may ever be.
 	DurationLimit          = big.NewInt(15)     // The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
 )
