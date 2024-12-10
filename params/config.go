39c26ecba84b4c9907e679c3f4b022d81fdb4321
Papiitoo <165153267+Papiitooo@users.noreply.github.com>
2024-12-10 22:49:51 +0100

    Update config.go

--- params/config.go
+++ params/config.go
@@ -27,7 +27,7 @@ import (
 
 // Genesis hashes to enforce below configs on.
 var (
-	MainnetGenesisHash   = common.HexToHash("0xbdeb7e975bd0f369be618f08d3384a75a408a50840532c547741bee9d84a8108")
+	MainnetGenesisHash   = common.HexToHash("0x1406880b4179c4470f1f45bbb9759ea09cb0e33731d1d8a007666276a606833c")
 	RopstenGenesisHash   = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d")
 	RinkebyGenesisHash   = common.HexToHash("0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177")
 	GoerliGenesisHash    = common.HexToHash("0xbf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a")
